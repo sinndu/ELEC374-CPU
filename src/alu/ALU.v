2bb47700be7652e3e6f55e148771c52e93fa20bc
RaelenGarces <raelengarces@gmail.com>
2026-02-03 19:19:32 -0500

    i fixed the ALU :,)

--- src/alu/ALU.v
+++ src/alu/ALU.v
@@ -1,95 +1,91 @@
 module ALU (
-    input [31:0] A, 
-    input [31:0] B,
+    input [31:0] A, B,
     input [4:0] shift_amount,
 
     //control signals
     input ADD, SUB, NEG,
-    input AND_op, OR_op, NOT_op,
+    input AND, OR, NOT,
     input SHR, SHL, SHRA, ROR, ROL,
     input MUL, DIV,
 
     //results
-    output reg [63:0] Z_data_in; // placehold for 64-bit Z register
+    output reg [63:0] Z_data_in
 );
     //internal wires to get results from sub-modules
     wire [31:0] add_sub_res;
-    wire [31:0] logic_res;
-    wire [31:0] shift_res;
     wire [63:0] mul_res;
-    wire [31:0] div_quotient;
-    wire [31:0] div_remainder;
+    wire [31:0] div_quotient, div_remainder;
     wire cout;
 
-	 // Addition/Subtraction
-     wire [31:0] actual_A = (NEG) ? 32'b0 : A; // If NEG is high, set A to 0 for negation
-
-    ALU_Add_Sub adder_unit ( 
-        .A(actual_A), 
+	 // Addition/Subtraction module
+    ALU_Add_Sub adder_unit (
+        .A((NEG) ? 32'b0 : A), 
         .B(B), 
         .sub_ctrl(SUB | NEG), 
         .result(add_sub_res), 
         .c_out(cout)
     );
-	 
-     // Logic unit 
-    ALU_LogicUnit logic_unit ( 
-        .A(A), 
-        .B(B), 
-        .AND(AND_op), 
-        .OR(OR_op), 
-        .NOT(NOT_op), 
-        .logic_result(logic_res)
-    );
-    
-     // Shifter
-    ALU_Shifter shifter_unit (
-        .A(A), 
-        .shift_amount(shift_amount), 
-        .SHR(SHR), 
-        .SHRA(SHRA), 
-        .SHL(SHL), 
-        .ROR(ROR), 
-        .ROL(ROL), 
-        .shift_result(shift_res)
-    );
 
 	 // Multiplication  
 	 Booth_Multiplier multiplier (
-        .M(A), 
-        .Q(B), 
-        .product(mul_res)
+        .M(A), .Q(B), .product(mul_res)
     );
 	 
 	 // Division
     NR_Division divider ( 
-        .Q(A), 
-        .M(B),
-        .quotient(div_quotient), 
-        .remainder(div_remainder)
+        .Q(A), .M(B), .quotient(div_quotient), .remainder(div_remainder)
     );
 
     // Final output multiplexer based on control signals
     always @(*) begin
+        //default to zero
+        Z_data_in = 64'b0;
+
+        // addition/subtraction/negation unit
         if (ADD || SUB || NEG) begin
-            Z_data_in = {32'b0, add_sub_res}; // Extend to 64 bits
+            Z_data_in = { {32{add_sub_res[31]}}, add_sub_res};
         end
-        else if (AND_op || OR_op || NOT_op) begin
-            Z_data_in = {32'b0, logic_res}; // Extend to 64 bits
+
+        // internal logic unit
+        else if (AND) begin
+            Z_data_in = {32'b0, A & B};
         end
-        else if (SHR || SHL || SHRA || ROR || ROL) begin
-            Z_data_in = {32'b0, shift_res}; // Extend to 64 bits
+        else if (OR) begin
+            Z_data_in = {32'b0, A | B};
         end
+        else if (NOT) begin 
+            Z_data_in = {32'b0, ~A};
+        end
+
+        // shift/rotate unit
+        else if (SHR) begin
+            Z_data_in = {32'b0, A >> shift_amount};
+        end
+        else if (SHRA) begin
+            Z_data_in = {32'b0, $signed(A) >>> shift_amount};
+        end
+        else if (SHL) begin
+            Z_data_in = {32'b0, A << shift_amount};
+        end
+        else if (ROR) begin
+            Z_data_in = {32'b0, (A >> shift_amount) | (A << (32 - shift_amount))};
+        end
+        else if (ROL) begin
+            Z_data_in = {32'b0, (A << shift_amount) | (A >> (32 - shift_amount))};
+        end
+        
+        // multiplication unit
         else if (MUL) begin
-            Z_data_in = mul_res; // Already 64 bits
+            Z_data_in = mul_res;
         end
+
+        // division unit
         else if (DIV) begin
             Z_data_in = {div_remainder, div_quotient}; // Remainder in upper 32 bits, Quotient in lower 32 bits
         end
-        else begin
+        
+        else
             Z_data_in = 64'b0; // Default case
-        end
     end
 
 endmodule
-    
