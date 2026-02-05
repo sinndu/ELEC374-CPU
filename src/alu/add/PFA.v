7885237a3ff2868d061579d6551501409aa87076
RaelenGarces <raelengarces@gmail.com>
2026-02-05 15:56:50 -0500

    merging my main into my branch

--- src/alu/add/PFA.v
+++ src/alu/add/PFA.v
@@ -1,8 +1,7 @@
-module PFS (
+module PFA (
     input a, b, cin,
     output s, g, p
 );
-
     assign s = a ^ b ^ cin; // sum
     assign g = a & b;       // generate
     assign p = a ^ b;       // propagate
